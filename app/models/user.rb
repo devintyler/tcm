fc7c231ebbe082d4ce9cf8eaffc7cc483671ccdd
Devin Visslailli <devin.viss@gmail.com>
2013-06-21 08:55:56 -0400

    add name to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,6 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :provider, :uid
   # attr_accessible :title, :body
+
+  attr_accessor:name
 end
